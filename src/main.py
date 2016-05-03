a1098cfb4cd23483c52f374e7a7ed3f4c85f3460
thoum <indiofish@gmail.com>
2016-05-03 14:40:36 +0900

    add main skeleton

---
+++ src/main.py
@@ -0,0 +1,8 @@
+def main():
+    # call parser
+    # pass the result to tokenizer
+    # evaluate the score from tokens
+    # print score
+    print("Hello, World!")
+if __name__ == '__main__':
+    main()
