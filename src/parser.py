2c6f2a832a78f439a39141abab4426c5c5e4dfba
thoum <indiofish@gmail.com>
2016-05-04 15:40:25 +0900

    remove tokenizer

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,8 @@
 import lexer
+# Kkma is slower, yet more verbose than Mecab.
+# If speed becomes the issue, consider changing to Mecab.
 from konlpy.tag import Kkma
-from konlpy.tag import Mecab
+# from konlpy.tag import Mecab
 
 nl_parser = Kkma()
 
--- src/tokenizer.py
+++
@@ -1,3 +0,0 @@
-# Kkma is slower, yet more verbose than Mecab.
-# If speed becomes the issue, consider changing to Mecab.
-from knlpy.tag import Kkma
