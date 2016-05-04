de9dafb9fcdbecbb516a57a34649f87c8847ed38
thoum <indiofish@gmail.com>
2016-05-04 15:51:43 +0900

    add error handling

--- src/main.py
+++ src/main.py
@@ -3,10 +3,13 @@ import parser
 
 def main():
     # call parser
-    fp = open("../tests/KakaoTalkChats.txt", 'r', encoding='utf-8')
-    chat = parser.parse(fp)
-    # pass the result to tokenizer
-    # evaluate the score from tokens
-    # print score
+    try:
+        fp = open("../tests/KakaoTalkChats.txt", 'r', encoding='utf-8')
+        chat = parser.parse(fp)
+        # pass the result to tokenizer
+        # evaluate the score from tokens
+        # print score
+    except IOError:
+        print("NO SUCH FILE")
 if __name__ == '__main__':
     main()
