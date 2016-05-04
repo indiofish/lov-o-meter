87398c84e033a3b77c5bbd12ccf6bd77d14768f9
thoum <indiofish@gmail.com>
2016-05-04 13:20:57 +0900

    recognize chat type from header and call appropriate parse function

--- src/parser.py
+++ src/parser.py
@@ -1,1 +1,21 @@
-# parses raw text file into chunks of consecutive dialogues
+# text log parser
+# import datetime
+KAKAO = "카카오톡"
+
+
+def parse(filename):
+    try:
+        f = open(filename, 'r', encoding='utf-8')
+        header = f.readline()
+        if KAKAO in header:
+            # consume extra 1 line
+            f.readline()
+            return __kakao_parser__(f)
+        else:
+            raise IOError
+    except IOError:
+        print("No such file or not defined")
+
+
+def __kakao_parser__(f):
+    print(f.readlines())
