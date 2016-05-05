9dafecd3bf7d07a4eace5d688efd302eecf5e93c
Thoum <indiofish@gmail.com>
2016-05-06 00:35:11 +0900

    use a queue to get result from a thread

--- src/main.py
+++ src/main.py
@@ -1,24 +1,33 @@
 import parser
+import queue
 import progress
 import threading
 
-def helper(fp):
-    chat = parser.parse(fp)
-    return chat
-
-def main():
+def helper(filename, queue):
     # TODO
     # call parser
     # pass the result to tokenizer
     # evaluate the score from tokens
     # print score
+    with open(filename, 'r', encoding='utf-8') as fp:
+        chat = parser.parse(fp)
+    queue.put(chat)
+    return
+
+
+def main():
+    que = queue.Queue()
+    filename = "../tests/KakaoTalkChats.txt"
     try:
-        fp = open("../tests/KakaoTalkChats.txt", 'r', encoding='utf-8')
-        th = threading.Thread(target=helper, args=(fp,))
+        th = threading.Thread(target=helper, args=(filename, que))
         proc_bar = threading.Thread(target=progress.show_progress, args=(th,))
-        th.start()
-        proc_bar.start()
+        th.start()  # start processing file data
+        proc_bar.start()  # draw process bar for user
+        th.join()
+        result = que.get()  # get result of helper function
+        print(result)
     except IOError:
         print("NO SUCH FILE")
+
 if __name__ == '__main__':
     main()
