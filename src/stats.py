67e9009a24ea741d420a2d4fa779078af705e52f
Thoum <indiofish@gmail.com>
2016-05-06 00:42:45 +0900

    skeleton for stats

---
+++ src/stats.py
@@ -0,0 +1,12 @@
+# with the parsed data, gather information
+class ChatData(object):
+    """docstring for ClassName"""
+    def __init__(self):
+        self.positive_cnt = 3
+        self.negative_cnt = 3
+
+
+def get_stats(chat):
+    # dummy value
+    tmp = ChatData()
+    return tmp
