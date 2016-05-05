2408c28174609c029647995a01c3ff14dc2a6d7c
Thoum <indiofish@gmail.com>
2016-05-06 02:17:41 +0900

    a go at concurrent tagging

--- src/parser.py
+++ src/parser.py
@@ -3,16 +3,37 @@ import jpype
 # Kkma is slower, yet more verbose than Mecab.
 # If speed becomes the issue, consider changing to Mecab.
 from konlpy.tag import Kkma
+from multiprocessing import Pool
+from threading import Thread
 # from konlpy.tag import Mecab
 
 nl_parser = Kkma()
 
 
-def parse(fp):
-    # to avoid error when ran as a thread
+def tagging(start, end, lines, result):
     jpype.attachThreadToJVM()
+    for i in range(start, end):
+        d = lines[i]
+        result.append((d[0], d[1], nl_parser.pos(d[2])))
+    return
+
+
+def parse(fp):
+    jpype.attachThreadToJVM()
+    # to avoid error when ran as a thread
     chat = lexer.lex(fp)
+    nlines = len(chat)
     ret = []
-    for data in chat:
-        ret.append((data[0], data[1], nl_parser.pos(data[2])))
+    t1 = Thread(target=tagging, args=(0, nlines//4, chat, ret))
+    t2 = Thread(target=tagging, args=(nlines//4+1, nlines//2, chat, ret))
+    t3 = Thread(target=tagging, args=(nlines//2+1, 3*(nlines//4), chat, ret))
+    t4 = Thread(target=tagging, args=(3*(nlines//4)+1, nlines, chat, ret))
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+    t1.join()
+    t2.join()
+    t3.join()
+    t4.join()
     return ret
