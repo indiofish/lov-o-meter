aeb890e940e42b56c27a436f7549536135f273d7
Thoum <indiofish@gmail.com>
2016-05-06 00:25:58 +0900

    use a wrapper for threading

--- src/init.py
+++
@@ -1,8 +0,0 @@
-import main
-import progress
-import threading
-
-th = threading.Thread(target=main.main)
-p = threading.Thread(target=progress.show_progress, args=(th,))
-th.start()
-p.start()
--- src/main.py
+++ src/main.py
@@ -1,15 +1,23 @@
 import parser
+import progress
+import threading
 
+def helper(fp):
+    chat = parser.parse(fp)
+    return chat
 
 def main():
+    # TODO
     # call parser
+    # pass the result to tokenizer
+    # evaluate the score from tokens
+    # print score
     try:
         fp = open("../tests/KakaoTalkChats.txt", 'r', encoding='utf-8')
-        chat = parser.parse(fp)
-        print(chat)
-        # pass the result to tokenizer
-        # evaluate the score from tokens
-        # print score
+        th = threading.Thread(target=helper, args=(fp,))
+        proc_bar = threading.Thread(target=progress.show_progress, args=(th,))
+        th.start()
+        proc_bar.start()
     except IOError:
         print("NO SUCH FILE")
 if __name__ == '__main__':
