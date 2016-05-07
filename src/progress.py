c2bef665bd3c2df733b0d4850d5e5302c6062f58
thoum <indiofish@gmail.com>
2016-05-07 19:08:13 +0900

    clear previous message when finished=True

--- src/progress.py
+++ src/progress.py
@@ -8,6 +8,17 @@ class ProgressBar(object):
         self.finished = False
         self.msg = msg
 
+    @property
+    def finished(self):
+        return self.__finished
+
+    @finished.setter
+    def finished(self, finished):
+        if finished:
+            # clear screen
+            print("\x1b[K", end='\r')
+        self.__finished = finished
+
     def show_progress(self):
         bar = Thread(target=self.__show_progress_dots__)
         bar.daemon = True
