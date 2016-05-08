bd20d9c35fc461bdff28814c26f793397fc6715e
thoum <indiofish@gmail.com>
2016-05-08 17:46:15 +0900

    add progress bar method

--- src/progress.py
+++ src/progress.py
@@ -7,6 +7,7 @@ class ProgressBar(object):
     def __init__(self, msg="Processing"):
         self.done = False
         self.msg = msg
+        self.progress = 0
 
     @property
     def done(self):
@@ -19,9 +20,19 @@ class ProgressBar(object):
             print("\x1b[K", end='\r')
         self.__done = done
 
+    @property
+    def progress(self):
+        return self.__progress
+
+    @progress.setter
+    def progress(self, progress):
+        if not 0 <= progress <= 100:
+            progress = 0
+        self.__progress = progress
+
     def show_progress(self):
         """create a seperate thread that draws a progress bar"""
-        bar = Thread(target=self.__show_progress_dots__)
+        bar = Thread(target=self.__show_progress_bars__)
         bar.daemon = True
         bar.start()
 
@@ -31,4 +42,9 @@ class ProgressBar(object):
         while not self.done:
             print("\x1b[K" + self.msg + "." * cnt, end='\r')
             cnt = (cnt + 1) % 4
+            sleep(1.0)
+
+    def __show_progress_bars__(self):
+        while not self.done:
+            print("\x1b[K{}".format(self.progress), end='\r')
             sleep(1.0)
