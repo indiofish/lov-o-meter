be393a1f1db3817a6781c74dc4245818855ea379
Thoum <indiofish@gmail.com>
2016-05-08 15:46:41 +0900

    make parser into a class

--- src/main.py
+++ src/main.py
@@ -6,15 +6,16 @@ from progress import ProgressBar
 
 def helper(filename):
     """parse file, get data, and evaluate rank"""
+    chat_parser = parser.ChatParser()
     with open(filename, 'r', encoding='utf-8') as fp:
-        chat = parser.parse(fp)
+        chat = chat_parser.parse(fp)
     chatdata = stats.get_stats(chat)
     ret = analyser.analyse(chatdata)
     return ret
 
 
 def main():
-    filename = "../tests/KakaoTalkChats.txt"
+    filename = "../tests/KakaoTalkChats1.txt"
     bar = ProgressBar()
     bar.show_progress()
     try:
--- src/parser.py
+++ src/parser.py
@@ -5,35 +5,38 @@ import jpype
 from konlpy.tag import Kkma
 from threading import Thread
 # from konlpy.tag import Mecab
-THREAD_CNT = 4
-
-nl_parser = Kkma()
 
 
-def tagging(result, que):
-    """get string from a queue and tags it"""
-    # to avoid error when ran as a thread
-    jpype.attachThreadToJVM()
-    while not que.empty():
-        tok = que.get()
-        try:
-            result[tok.pos] = (tok.time, tok.user,
-                               nl_parser.pos(tok.contents))
-        except AttributeError:
-            pass
-    return
+class ChatParser(object):
+    """docstring for ChatParser"""
+    def __init__(self):
+        self.tagger = Kkma()
+        self.thread_cnt = 4
+        self.progress = 0  # 0 ~ 100
 
+    def tagging(self, result, que):
+        """get string from a queue and tags it"""
+        # to avoid error when ran as a thread
+        jpype.attachThreadToJVM()
+        while not que.empty():
+            tok = que.get()
+            try:
+                result[tok.pos] = (tok.time, tok.user,
+                                   self.tagger.pos(tok.contents))
+            except AttributeError:
+                pass
+        return
 
-def parse(fp):
-    """create threads to tag chatlog using a thread-safe queue"""
-    chat_len, chat_que = lexer.lex(fp)
-    ret = [None] * chat_len
-    pool = [Thread(target=tagging, args=(ret, chat_que))
-            for _ in range(THREAD_CNT)]
-    for t in pool:
-        t.daemon = True
-        t.start()
-    for t in pool:
-        t.join()
-    # print(ret)
-    return ret
+    def parse(self, fp):
+        """create threads to tag chatlog using a thread-safe queue"""
+        chat_len, chat_que = lexer.lex(fp)
+        ret = [None] * chat_len
+        pool = [Thread(target=self.tagging, args=(ret, chat_que))
+                for _ in range(self.thread_cnt)]
+        for t in pool:
+            t.daemon = True
+            t.start()
+        for t in pool:
+            t.join()
+        # print(ret)
+        return ret
