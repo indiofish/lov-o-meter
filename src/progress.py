00f7d8fd278bb09ce23181113c92100c53fa23ec
Thoum <indiofish@gmail.com>
2016-05-09 22:33:55 +0900

    add drawing bar function

--- src/progress.py
+++ src/progress.py
@@ -5,6 +5,7 @@ HIDE_CURSOR = '\x1b[?25l'
 SHOW_CURSOR = '\x1b[?25h'
 CLEAR_LINE = '\x1b[K'
 
+
 class ProgressBar(object):
     """progressbar ui drawer"""
     def __init__(self, msg="Processing", full=100):
@@ -12,6 +13,7 @@ class ProgressBar(object):
         self.msg = msg
         self.progress = 0
         self.full = full
+        self.bar = None
 
     @property
     def done(self):
@@ -19,27 +21,34 @@ class ProgressBar(object):
 
     @done.setter
     def done(self, done):
+        self.__done = done
         if done:
+            self.bar.join()
             # clear screen
             print(SHOW_CURSOR+CLEAR_LINE, end='\r')
-        self.__done = done
 
     def show_progress(self):
         """create a seperate thread that draws a progress bar"""
-        bar = Thread(target=self.__show_progress_bars__)
-        bar.daemon = True
-        bar.start()
+        self.bar = Thread(target=self.__show_progress_bars__)
+        self.bar.daemon = True
+        self.bar.start()
 
     def __show_progress_dots__(self):
         """draws 3 dots until done"""
         cnt = 0
         while not self.done:
-            print("\x1b[K" + self.msg + "." * cnt, end='\r')
+            print(CLEAR_LINE + self.msg + "." * cnt, end='\r')
             cnt = (cnt + 1) % 4
             sleep(1.0)
 
     def __show_progress_bars__(self):
+        """draws filling bars until done"""
+        max_bar = 40
+        fill_char = '|'
         while not self.done:
-            progress = int((self.progress / self.full) * 100)
-            print(HIDE_CURSOR+CLEAR_LINE+str(progress), end='\r')
+            progress = (self.progress / self.full)
+            bar = ("[" +
+                   (fill_char*int(max_bar * progress)).ljust(max_bar) +
+                   "]")
+            print(HIDE_CURSOR+CLEAR_LINE+self.msg, bar, end='\r')
             sleep(0.5)
